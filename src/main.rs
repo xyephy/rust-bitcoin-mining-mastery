73e5f40ade82edd4c761b54735ffefab004b4e94
Толли <ziphyr.xyeag@gmail.com>
2025-03-04 22:09:18 +0300

    Day 3: update borrowing

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,9 @@
+fn print_miner(name: &String) { // Borrow, don’t take
+    println!("Borrowed miner: {}", name);
+}
+
 fn main() {
-    let miner = String::from("Satoshi"); // Owned string
-    println!("Miner: {}", miner);
-
-   let miner2 = miner; // Move (not copy!)
-   // println!("Miner: {}", miner); // This errors—miner’s gone!
-    println!("Miner2: {}", miner2);
-
-    let hash_rate = 42; // Primitive, copies fine
-    let hash_rate2 = hash_rate; // No move here
-    println!("Hash rates: {} and {}", hash_rate, hash_rate2);
+    let miner = String::from("Satoshi");
+    print_miner(&miner); // Pass reference
+    println!("Still got miner: {}", miner); // Works!
 }
